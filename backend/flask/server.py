464f29ca9264f2eccb68850ba3c0b17a724e3ad7
VC-cyber <77545395+VC-cyber@users.noreply.github.com>
2024-11-13 17:15:45 -0800

    server.py and routes added

--- backend/flask/server.py
+++ backend/flask/server.py
@@ -60,6 +60,9 @@ def logout():
 def home():
     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3000))

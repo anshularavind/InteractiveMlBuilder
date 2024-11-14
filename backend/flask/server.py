2c27f1f416c7d5bde8c6914d8a2f7c3527353f3f
Noah Cylich <noahcylich@gmail.com>
2024-11-13 17:42:41 -0800

    configured auth0 with https, and proper log in and logout handling

--- backend/flask/server.py
+++ backend/flask/server.py
@@ -4,7 +4,9 @@ from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, redirect, render_template, session, url_for, request
+from functools import wraps
+
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -33,11 +35,22 @@ def login():
     )
 
 
+def requires_auth(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            session['next_url'] = request.url
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route("/callback", methods=["GET", "POST"])
 def callback():
     token = oauth.auth0.authorize_access_token()
     session["user"] = token
-    return redirect("/")
+    next_url = session.pop('next_url', url_for('home'))
+    return redirect(next_url)
 
 
 @app.route("/logout")
@@ -60,10 +73,18 @@ def logout():
 def home():
     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
+
 @app.route('/hello')
+@requires_auth
 def hello():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=env.get("PORT", 3000))
-
+    app.run(
+        host="0.0.0.0",
+        port=int(env.get("PORT", 3000)),
+        ssl_context=(
+            env.get("SSL_CERT_PATH"),  # Path to your certificate
+            env.get("SSL_KEY_PATH")    # Path to your private key
+        )
+    )

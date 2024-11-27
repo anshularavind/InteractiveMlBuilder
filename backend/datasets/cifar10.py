966af60eff4b8de58b061ff6d3483138ffe9375e
ammesatyajit <ammesatyajit@gmail.com>
2024-11-27 01:34:02 -0800

    add cifar10 and bug fix for mnist

--- backend/datasets/cifar10.py
+++ backend/datasets/cifar10.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from datasets import load_dataset
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
@@ -7,8 +7,7 @@ from torch import nn
 from backend.ml.train import train_model
 import math
 
-
-class MNISTDataset(Dataset):
+class CIFAR10Dataset(Dataset):
     def __init__(self, data, transform=None):
         self.data = data
         self.transform = transform
@@ -17,28 +16,27 @@ class MNISTDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        image = self.data[idx]["image"]
+        image = self.data[idx]["img"]
         label = self.data[idx]["label"]
 
         # Apply the transformation, if any
         if self.transform:
-            image = numpy.array(image)
-            image = torch.tensor(image, dtype=torch.float32).unsqueeze(0)
-            image = self.transform(image).squeeze(0)
+            image = np.array(image)
+            image = torch.tensor(image, dtype=torch.float32).permute(2, 0, 1)
+            image = self.transform(image)
 
-        # flattening and getting rid of channel
-        H, W = image.size()
-        image = image.reshape(H * W)
+        C, H, W = image.size()
+        image = image.reshape(C * H * W)
 
         return image, label
 
-class Mnist:
-    dataset = load_dataset("ylecun/mnist")
+class Cifar10:
+    dataset = load_dataset("cifar10")
     criterion = nn.CrossEntropyLoss()
-    num_channels = 1
+    num_channels = 3
 
     def __init__(self, batch_size=64):
-        self.train_loader, self.test_loader = Mnist.__get_mnist_data_loaders(batch_size=batch_size)
+        self.train_loader, self.test_loader = Cifar10.__get_cifar10_data_loaders(batch_size=batch_size)
         self.batch_size = batch_size
 
     def get_output_size(self):
@@ -51,15 +49,12 @@ class Mnist:
         correct = (predicted == target).sum().item()
         return correct, total
 
-
     @staticmethod
-    def __get_mnist_data_loaders(batch_size=64):
+    def __get_cifar10_data_loaders(batch_size=64):
+        transform = transforms.Compose([transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
 
-        # Step 3: Define transformations (normalize values to [-1, 1])
-        transform = transforms.Compose([transforms.Normalize((0.1307,), (0.3081,))])
-
-        train_dataset = MNISTDataset(Mnist.dataset["train"], transform=transform)
-        test_dataset = MNISTDataset(Mnist.dataset["test"], transform=transform)
+        train_dataset = CIFAR10Dataset(Cifar10.dataset["train"], transform=transform)
+        test_dataset = CIFAR10Dataset(Cifar10.dataset["test"], transform=transform)
 
         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
         test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
@@ -68,32 +63,33 @@ class Mnist:
 
 if __name__ == '__main__':
     class Pre_Process(nn.Module):
-        def __init__(self):
+        def __init__(self, num_channels=3):
             super(Pre_Process, self).__init__()
+            self.num_channels = num_channels
 
         def forward(self, x):
-            side_squared = x.size(-1)
+            side_squared = x.size(-1) / self.num_channels
             side = math.floor(math.sqrt(side_squared))
             assert side * side == side_squared, "Image must be square"
-            x = x.reshape(-1, 1, side, side)
+            x = x.reshape(-1, 3, side, side)
             return x
 
     class ExampleModel(nn.Module):
         def __init__(self):
             super(ExampleModel, self).__init__()
-            self.dataset = Mnist()
+            self.dataset = Cifar10()
             self.epochs = 5
             self.lr = 0.001
             self.model = nn.Sequential(
                 Pre_Process(),
-                nn.Conv2d(1, 32, kernel_size=5),
+                nn.Conv2d(3, 32, kernel_size=5),
                 nn.ReLU(),
                 nn.MaxPool2d(2),
                 nn.Conv2d(32, 64, kernel_size=5),
                 nn.ReLU(),
                 nn.MaxPool2d(2),
                 nn.Flatten(),
-                nn.Linear(1024, 128),
+                nn.Linear(1600, 128),
                 nn.ReLU(),
                 nn.Linear(128, 10)
             )
@@ -106,4 +102,3 @@ if __name__ == '__main__':
 
     model = ExampleModel()
     train_model(model, epochs=5)
-

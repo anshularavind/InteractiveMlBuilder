dff9eef2709244a202ae92a57c884891d8737dcd
svshirsa <sshirsathe2023@gmail.com>
2024-11-27 02:06:16 -0800

    created .css files

--- frontend/src/About/About.js
+++ frontend/src/About/About.js
@@ -1,42 +1,13 @@
 import React from "react";
-
+import './About.css'; 
 function About({ goBack }) {
   return (
-    <div style={styles.container}>
+    <div style={"container"}>
      
-      <h2 style={styles.header}>Sooooo This Appp BLAH BLAh</h2>
+      <h2 style={"header"}>Sooooo This Appp BLAH BLAh</h2>
     </div>
   );
 }
 
-const styles = {
-  container: {
-    textAlign: "center",
-    padding: "20px",
-    fontFamily: "'Poppins', sans-serif",
-    backgroundColor: "#f8f9fa",
-    minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  goBackButton: {
-    position: "absolute",
-    top: "20px",
-    left: "20px",
-    fontSize: "1rem",
-    padding: "10px 15px",
-    color: "#fff",
-    backgroundColor: "#4a90e2",
-    border: "none",
-    borderRadius: "8px",
-    cursor: "pointer",
-  },
-  header: {
-    fontSize: "2rem",
-    color: "#4a90e2",
-  },
-};
 
 export default About;

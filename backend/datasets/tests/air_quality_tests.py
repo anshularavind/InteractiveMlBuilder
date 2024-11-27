fc0033f210fb636874bc6626fe0e607a34dea811
Noah Cylich <noahcylich@gmail.com>
2024-11-27 12:54:14 -0800

    automatically creates log files and adding trailing changes from previous dataset testing scripts

---
+++ backend/datasets/tests/air_quality_tests.py
@@ -0,0 +1,54 @@
+from backend.datasets.air_quality import AirQuality
+from backend.ml.block_builder import BuiltModel
+from backend.ml.train import train_model
+import math
+
+
+def init_db():
+    # user_db = UserDatabase()
+    # user_db.clear()
+    # user_db.delete()
+    # user_db = UserDatabase()
+    user_db = None
+    return user_db
+
+
+def test_air_quality_nn_model():
+    air_quality_nn_model = '''{
+        "input": 11,
+        "output": 2,
+        "dataset": "AirQuality",
+        "LR": "0.001",
+        "batch_size": 512,
+        "blocks": [
+            {
+                "block": "FcNN",
+                "params": {
+                    "output_size": 128,
+                    "hidden_size": 256,
+                    "num_hidden_layers": 2
+                }
+            },
+            {
+                "block": "FcNN",
+                "params": {
+                    "output_size": 64,
+                    "hidden_size": 128,
+                    "num_hidden_layers": 2
+                }
+            },
+            {
+                "block": "FcNN",
+                "params": {
+                    "output_size": 2,
+                    "hidden_size": 64,
+                    "num_hidden_layers": 1
+                }
+            }
+        ]
+    }'''
+
+    # training the model to test basic functionality
+    model = BuiltModel(air_quality_nn_model, 'test_user', init_db(), '..')
+    result = train_model(model, 2)
+    assert not math.isnan(result), 'Model training failed'

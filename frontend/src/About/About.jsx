92e7d63fb456bb944b69f76c33c2b4bff7b18bf5
svshirsa <sshirsathe2023@gmail.com>
2024-11-27 09:54:17 -0800

    fixed aboutScreen

---
+++ frontend/src/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './About.css'; // Import external CSS file
+
+function About() {
+  return (
+    <div className="container"> 
+      <h2 className="header">SOOOO thisss person</h2> 
+    </div>
+  );
+}
+
+export default About;

552775b4a176a5471b6f82bf1d4342b1ae10313e
Noah Cylich <noahcylich@gmail.com>
2024-12-01 18:47:54 -0800

    switched backend to http to fix cert errors

--- frontend/src/ModelBuilder/ModelBuilder.js
+++ frontend/src/ModelBuilder/ModelBuilder.js
@@ -92,7 +92,7 @@ function ModelBuilder() {
     try {
       const token = await getAccessTokenSilently();
 
-      const response = await fetch("https://localhost:4000/api/define-model", {
+      const response = await fetch("http://127.0.0.1:4000/api/define-model", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

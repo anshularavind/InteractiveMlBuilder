038bfaf414d9e383e8760b9e25e468806d778701
Noah Cylich <noahcylich@gmail.com>
2024-12-02 17:30:02 -0800

    adding training section to frontend and made config column better

--- frontend/src/ModelBuilder/ConfigColumn/Components/Train.jsx
+++ frontend/src/ModelBuilder/ConfigColumn/Components/Train.jsx
@@ -1,11 +1,42 @@
 import "../../ModelBuilder.css";
 
-function Train() {
-  return (
-    <div>
-      <h4>Training Section</h4>
-    </div>
-  );
+function Train({ trainInputs, handleInputChange }) {
+    return (
+        <div>
+            <h2>Training Params</h2>
+            <div>
+                <label>Learning Rate: </label>
+                <br/>
+                <input
+                    type="number"
+                    value={trainInputs.lr}
+                    onChange={(e) => handleInputChange("lr", e.target.value)}
+                    className="input"
+                />
+            </div>
+            <div>
+                <label>Batch Size: </label>
+                <br/>
+                <input
+                    type="number"
+                    value={trainInputs.batch_size}
+                    onChange={(e) => handleInputChange("batch_size", e.target.value)}
+                    className="input"
+                />
+            </div>
+            <div>
+                <label>Epochs: </label>
+                <br/>
+                <input
+                    type="number"
+                    value={trainInputs.epochs}
+                    onChange={(e) => handleInputChange("epochs", e.target.value)}
+                    className="input"
+                />
+            </div>
+            <br/>
+        </div>
+    );
 }
 
 export default Train;

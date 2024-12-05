75125f750d9a44354e7a39af87a2a1783156212a
Noah Cylich <noahcylich@gmail.com>
2024-12-04 17:23:18 -0800

    fixed visualization for 0 hidden layers

--- frontend/src/ModelBuilder/ConfigColumn/ConfigColumn.jsx
+++ frontend/src/ModelBuilder/ConfigColumn/ConfigColumn.jsx
@@ -230,8 +230,12 @@ function ConfigColumn({
     let newLayer;
 
     if (selectedLayer === "FcNN") {
-      const hiddenSizeScaled =
-        (Math.log((blockInputs.hiddenSize + 1) || 1) * SCALING_CONSTANT) / log_base;
+      let hiddenSizeScaled;
+      if (blockInputs.numHiddenLayers === 0) {
+        hiddenSizeScaled = (inputSizeScaled + outputSizeScaled) / 2;
+      } else {
+        hiddenSizeScaled = (Math.log((blockInputs.hiddenSize + 1) || 1) * SCALING_CONSTANT) / log_base;
+      }
       const numHiddenLayersScaled = blockInputs.numHiddenLayers * SCALING_CONSTANT;
       const trapHeight = SCALING_CONSTANT * 2;
 
